a25fd86cdf157ce8efdc6807e6a62bfc282337fa
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-21 20:11:46 +0500

    updated Odd and even program so it now also prints odd numbers as well

--- Odd&Even.cpp
+++ Odd&Even.cpp
@@ -3,41 +3,31 @@ using namespace std;
 
 int main() {
     int start, end;
-    long long oddProduct = 1;
-    bool oddFound = false;  // Flag to track if any odd number is found
-
-    // Input starting and ending numbers
-    cout << "Enter starting number: ";
+    long long product = 1; 
+    
+    cout << "Enter Your Starting Number: " << endl;
     cin >> start;
-    cout << "Enter ending number: ";
+    
+    cout << "Enter Your Ending Number: " << endl;
     cin >> end;
-
-    // Printing even numbers between start and end
-    cout << "Even numbers between " << start << " and " << end << " are:" << endl;
+    
+    cout << "The even numbers are: ";
     for (int i = start; i <= end; i++) {
         if (i % 2 == 0) {
-            cout << i << ", ";
+            cout << i << " , ";
         }
     }
-    cout << endl;
-
-    // Calculate the product of odd numbers between start and end
-    cout << "Product of odd numbers between " << start << " and " << end << " is:" << endl;
-    for (int i = start; i <= end; i++) {
-        if (i % 2 != 0) {
-        	cout<<i<<"*";
-            oddProduct *= i;
-            oddFound = true;  // Set flag if an odd number is found
+    
+    cout << "\nThe odd numbers are: ";
+    for (int j = start; j <= end; j++) {
+        if (j % 2 != 0) {
+            cout << j << " , ";
+            product *= j;
         }
     }
-
-    // If any odd number was found, print the product
-    if (oddFound) {
-        cout <<endl<< oddProduct << endl;
-    } else {
-        cout << "No odd numbers found." << endl;
-    }
-
+    
+    cout << "\nThe product of odd numbers is: " << product << endl;
+    
     return 0;
 }
 
