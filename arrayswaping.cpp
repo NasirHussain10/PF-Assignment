51a5c4666e32a8ccc84addb1620015ecad7023da
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-22 20:11:12 +0500

    Sum of even elementsin arrays added

--- arrayswaping.cpp
+++ arrayswaping.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
 using namespace std;
   int main(){
-  	int arr[3],arr2[3];
-  	
-  	cout<<"Array 1 : "<<endl<<endl;
-  	
+  	int arr[3],arr2[3],sum=0;
+
   	for(int i=0 ; i<3 ; i++){
   		cout<<"Enter element "<<i+1<<" : "<<endl;
   		cin>>arr[i];
   		arr2[i]=arr[i];
 	  }
-	  
-	  cout<<"\n\nArray 2 values:"<<endl<<endl;
-	  
-	  for(int j=0 ; j<3 ; j++){
-	  	cout<<"Element "<<j+1<<" : "<<arr2[j]<<endl;
+	  cout<<"The elements stored in first array are: ";
+	  for(int k=0 ; k<3 ; k++){
+	    cout<<arr[k]<<" , ";
 	  }
+	  cout<<"\n\nThe elements copied into second array are: ";
+	  for(int j=0 ; j<3 ; j++){
+	  	cout<<arr2[j]<<" , ";
+	  }
+	  cout<<"\n\nSum: ";
+	  for(int a=0 ; a<3 ; a++){
+	  	if(arr2[a]%2==0){
+	  		cout<<arr2[a]<<" + ";
+	  		sum=sum+arr2[a];
+		  }
+	  }
+	  cout<<" = "<<sum;
   	return 0;
   }
