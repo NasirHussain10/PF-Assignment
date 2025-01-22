a0eb481e1dadf39a00e89552dbe447fcf9dde834
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-22 10:17:29 +0500

    pattern 1 completed

---
+++ pattern1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+   int main(){
+   	int i=1 , num=0;
+   	while(i<=5){
+   		num=0;
+   		while(num<i){
+   			cout<<i;
+   			num++;
+		   }
+		   cout<<endl;
+		   i++;
+	   }
+   }
