f04bbabd215e4b930552590b7f689063730a0a35
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-26 22:17:00 +0500

    program which defines a UDF table(). This UDF takes int array of size 5 input from user in its body and prints the table of each number

---
+++ Question19.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void table(int arr[]) {
+    for (int i = 0; i < 5; i++) {
+        cout << "Table of " << arr[i] << ":\n";
+        for (int j = 1; j <= 10; j++) {
+            cout << arr[i] << " x " << j << " = " << arr[i] * j << endl;
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int arr[5];
+    
+    cout << "Enter 5 integers: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    table(arr);
+
+    return 0;
+}
