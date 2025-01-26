6733e535295e0c9a1b4ed1330b52eef728a167f1
NasirHussain10 <nasirhussian.asadi125@gmail.com>
2025-01-26 21:18:14 +0500

    Converted the program from C++ to C

--- Question7.cpp
+++ Question7.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
-using namespace std;
-  int main(){
-  	int arr[3],arr2[3],sum=0;
+#include <stdio.h>
 
-  	for(int i=0 ; i<3 ; i++){
-  		cout<<"Enter element "<<i+1<<" : "<<endl;
-  		cin>>arr[i];
-  		arr2[i]=arr[i];
-	  }
-	  cout<<"The elements stored in first array are: ";
-	  for(int k=0 ; k<3 ; k++){
-	    cout<<arr[k]<<" , ";
-	  }
-	  cout<<"\n\nThe elements copied into second array are: ";
-	  for(int j=0 ; j<3 ; j++){
-	  	cout<<arr2[j]<<" , ";
-	  }
-	  cout<<"\n\nSum: ";
-	  for(int a=0 ; a<3 ; a++){
-	  	if(arr2[a]%2==0){
-	  		cout<<arr2[a]<<" + ";
-	  		sum=sum+arr2[a];
-		  }
-	  }
-	  cout<<" = "<<sum;
-  	return 0;
-  }
+int main() {
+    int arr[3], arr2[3], sum = 0;
+
+    for(int i = 0; i < 3; i++) {
+        printf("Enter element %d :\n", i + 1);
+        scanf("%d", &arr[i]);
+        arr2[i] = arr[i];
+    }
+
+    printf("The elements stored in first array are: ");
+    for(int k = 0; k < 3; k++) {
+        printf("%d , ", arr[k]);
+    }
+
+    printf("\n\nThe elements copied into second array are: ");
+    for(int j = 0; j < 3; j++) {
+        printf("%d , ", arr2[j]);
+    }
+
+    printf("\n\nSum: ");
+    for(int a = 0; a < 3; a++) {
+        if(arr2[a] % 2 == 0) {
+            printf("%d + ", arr2[a]);
+            sum = sum + arr2[a];
+        }
+    }
+
+    printf("= %d\n", sum);
+
+    return 0;
+}
